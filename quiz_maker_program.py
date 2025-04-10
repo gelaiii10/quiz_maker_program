4da2b4cec5e675b6d4ac10b0ebdc370018acbb19
gelaiii10 <gelaibacleaan@gmail.com>
2025-04-09 22:40:11 -0800

    while loop to continue collectiong questions and answer

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -1,4 +1,10 @@
 def get_valid_answer(prompt):
     # docstring to explain the purpose of the function
     """Function to get a valid answer option from the user"""   
-    
+    while True: # while loop to continue collecting questions and answers until the user types 'exit'
+        answer = input(prompt).lower()
+        if answer in ['a', 'b', 'c', 'd']:
+            return answer
+        else:
+            # if the input is invalid, it will print an error message and prompt the user to enter again.
+            print("Invalid input. Please enter a valid option (a, b, c, or d)") 
