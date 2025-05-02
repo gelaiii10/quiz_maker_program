acc138640ada73b6bf1224be5b61a0e113c2b630
gelaiii10 <gelaibacleaan@gmail.com>
2025-05-01 19:43:52 -0800

    function that takes a list of questions as input and randomly select a question and its correct answer the print the selected question and answer

--- quiz_program_that_read_output_file_of_the_quiz.py
+++ quiz_program_that_read_output_file_of_the_quiz.py
@@ -4,8 +4,14 @@ import random # import module
 def load_questions(filename):
     questions = [] # initialize and empty list
     with open(filename, "r") as file: # open the specified file for reaing
+        
         # each line in the file, it splits the line into a question and answer using the semicolon as a delimeter
         for line in file:
             question, answer = line.strip().split(";")
             questions.append((question, answer))  # it appends the tuple to the list
             return questions  # close the file and returns the list of the questions
+        
+def quiz_user(questions): # this function takes a list of questions as input
+    # randomly selects a question and its corresponding correct answer from the list
+    question, correct_answer = random.choice(questions)
+    print("questions: " + question)  # prints the selected questions and promts the user for their answer
